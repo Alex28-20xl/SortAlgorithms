ff21f7a1d39f5c63758259cbce3734b8735fe912
Alexsandr <griefgiver@yandex.ru>
2021-09-16 16:05:59 +0300

    Реализована сортировка двоичным деревом поиска

---
+++ Algorithm/TreeSort.cs
@@ -0,0 +1,17 @@
+﻿using Algorithm.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public class TreeSort<T>:AlgorithmBase<T> where T:IComparable
+    {
+        protected override void MakeSort()
+        {
+            var tree = new Tree<T>(Items);
+            var sorted = tree.Inorder();
+            Items = sorted;
+        }
+    }
+}
